ffc93f047f14c5204bf6d4657067d56ab790bd4d
fer-ramirez21 <mr2395446@gmail.com>
2025-05-16 11:20:18 -0600

    Create Factura.java
    Seción 5

---
+++ Factura.java
@@ -0,0 +1,30 @@
+// Factura.java
+public class Factura {
+    private String folio;
+    private String cliente;
+    private double total;
+
+    public Factura(String folio, String cliente, double total) {
+        this.folio = folio;
+        this.cliente = cliente;
+        this.total = total;
+    }
+
+    @Override
+    public String toString() {
+        return "🧾 Factura [folio=" + folio + ", cliente=" + cliente + ", total=$" + total + "]";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Factura otra = (Factura) obj;
+        return folio.equals(otra.folio);
+    }
+
+    @Override
+    public int hashCode() {
+        return folio.hashCode();
+    }
+}
